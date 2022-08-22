4916036f53f1624967536f69ede292b7c694ada2
pulokmehrab <khandokarmeherab@gmail.com>
2022-08-22 17:53:23 +0600

    player nammee done

--- js/selection.js
+++ js/selection.js
@@ -1,3 +1,95 @@
+ document.getElementById('mbappy').addEventListener('click',function(){
+    // console.log("mabappy");
+
+   
+    const playername = document.getElementById('player-name1');
+    const playerName = playername.innerText;
+    playername.innerText =playerName;
+     const playerList = document.getElementById('select-players');
+    //  console.log(playerList);
+    const li1 =document.createElement('li');
+    li1.innerText =playerName;
+    playerList.appendChild(li1);
+
+ })
+
+ document.getElementById('ronaldo').addEventListener('click',function(){
+   
+    const playername = document.getElementById('player-name2');
+    const playerName = playername.innerText;
+    playername.innerText =playerName;
+     const playerList = document.getElementById('select-players');
+    
+    const li1 =document.createElement('li');
+    li1.innerText =playerName;
+    playerList.appendChild(li1);
+
+ })
+ document.getElementById('messi').addEventListener('click',function(){
+   
+    const playername = document.getElementById('player-name3');
+    const playerName = playername.innerText;
+    playername.innerText =playerName;
+     const playerList = document.getElementById('select-players');
+    
+    const li1 =document.createElement('li');
+    li1.innerText =playerName;
+    playerList.appendChild(li1);
+
+ })
+ document.getElementById('sala').addEventListener('click',function(){
+   
+    const playername = document.getElementById('player-name4');
+    const playerName = playername.innerText;
+    playername.innerText =playerName;
+     const playerList = document.getElementById('select-players');
+    
+    const li1 =document.createElement('li');
+    li1.innerText =playerName;
+    playerList.appendChild(li1);
+
+ })
+ document.getElementById('romero').addEventListener('click',function(){
+   
+    const playername = document.getElementById('player-name5');
+    const playerName = playername.innerText;
+    playername.innerText =playerName;
+     const playerList = document.getElementById('select-players');
+    
+    const li1 =document.createElement('li');
+    li1.innerText =playerName;
+    playerList.appendChild(li1);
+
+ })
+ document.getElementById('hulk').addEventListener('click',function(){
+   
+    const playername = document.getElementById('player-name6');
+    const playerName = playername.innerText;
+    playername.innerText =playerName;
+     const playerList = document.getElementById('select-players');
+    
+    const li1 =document.createElement('li');
+    li1.innerText =playerName;
+    playerList.appendChild(li1);
+
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 document.getElementById('calculation').addEventListener('click',function(){
     const playerField=document.getElementById('per-player');
    const playerFieldString =playerField.value;
