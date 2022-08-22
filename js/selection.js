7504185542340b496269d0cb73abe3e1afc35d9c
pulokmehrab <khandokarmeherab@gmail.com>
2022-08-22 21:26:52 +0600

    alert

--- js/selection.js
+++ js/selection.js
@@ -1,139 +1,86 @@
- document.getElementById('mbappy').addEventListener('click',function(){
-    // console.log("mabappy");
+function players(playerId) {
+   const playername = document.getElementById(playerId);
+   const playerName = playername.innerText;
+   playername.innerText = playerName;
+   const playerList = document.getElementById("select-players");
+   //  console.log(playerList);
+   const li1 = document.createElement("li");
+   li1.innerText = playerName;
+   if (playerList.childElementCount <= 4) {
+     playerList.appendChild(li1);
+   } else {
+     alert("You can't add more");
+   }
+ }
+ 
+ document.getElementById("mbappy").addEventListener("click", function () {
+   // console.log("mabappy");
+   players("player-name1");
 
-   
-    const playername = document.getElementById('player-name1');
-    const playerName = playername.innerText;
-    playername.innerText =playerName;
-     const playerList = document.getElementById('select-players');
-    //  console.log(playerList);
-    const li1 =document.createElement('li');
-    li1.innerText =playerName;
-    playerList.appendChild(li1);
-
- })
-
- document.getElementById('ronaldo').addEventListener('click',function(){
-   
-    const playername = document.getElementById('player-name2');
-    const playerName = playername.innerText;
-    playername.innerText =playerName;
-     const playerList = document.getElementById('select-players');
-    
-    const li1 =document.createElement('li');
-    li1.innerText =playerName;
-    playerList.appendChild(li1);
-
- })
- document.getElementById('messi').addEventListener('click',function(){
-   
-    const playername = document.getElementById('player-name3');
-    const playerName = playername.innerText;
-    playername.innerText =playerName;
-     const playerList = document.getElementById('select-players');
-    
-    const li1 =document.createElement('li');
-    li1.innerText =playerName;
-    playerList.appendChild(li1);
-
- })
- document.getElementById('sala').addEventListener('click',function(){
-   
-    const playername = document.getElementById('player-name4');
-    const playerName = playername.innerText;
-    playername.innerText =playerName;
-     const playerList = document.getElementById('select-players');
-    
-    const li1 =document.createElement('li');
-    li1.innerText =playerName;
-    playerList.appendChild(li1);
-
- })
- document.getElementById('romero').addEventListener('click',function(){
-   
-    const playername = document.getElementById('player-name5');
-    const playerName = playername.innerText;
-    playername.innerText =playerName;
-     const playerList = document.getElementById('select-players');
-    
-    const li1 =document.createElement('li');
-    li1.innerText =playerName;
-    playerList.appendChild(li1);
-
- })
- document.getElementById('hulk').addEventListener('click',function(){
-   
-    const playername = document.getElementById('player-name6');
-    const playerName = playername.innerText;
-    playername.innerText =playerName;
-     const playerList = document.getElementById('select-players');
-    
-    const li1 =document.createElement('li');
-    li1.innerText =playerName;
-    playerList.appendChild(li1);
-
- })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-document.getElementById('calculation').addEventListener('click',function(){
-    const playerField=document.getElementById('per-player');
-   const playerFieldString =playerField.value;
-   const playercost =parseInt(playerFieldString);
-   const fiveplayerCost = 5*playercost;
-
-//    playercost....
-
-const expensesField= document.getElementById('player-expense');
-const expensesFieldValue =expensesField.innerText;
-expensesField.innerText=fiveplayerCost;
-
-
-
-// manager cost....
-
-
-document.getElementById('Total-calc').addEventListener('click',function(){
-
-    const managerField =document.getElementById('manager');
-    const managerFieldString =managerField.value;
-    const managerCost=parseInt(managerFieldString);
-    managerField.value=managerCost;
-    // console.log(managerCost);
-//    coach costing
-    const coachField =document.getElementById('coach-cost');
-    const coachFieldString =coachField.value;
-    const coachCoast =parseInt(coachFieldString);
-    coachField.value =coachCoast;
-    
-    const totalCoast =fiveplayerCost+managerCost+coachCoast;
-    //    console.log(totalCoast);
-
+ });
+ 
+ document.getElementById("ronaldo").addEventListener("click", function () {
+   players("player-name2");
+ });
+ document.getElementById("messi").addEventListener("click", function () {
+   players("player-name3");
+ });
+ document.getElementById("sala").addEventListener("click", function () {
+   players("player-name4");
+ });
+ document.getElementById("romero").addEventListener("click", function () {
+   players("player-name5");
+ });
+ document.getElementById("hulk").addEventListener("click", function () {
+   players("player-name6");
+ });
+ 
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ document.getElementById("calculation").addEventListener("click", function () {
+   const playerField = document.getElementById("per-player");
+   const playerFieldString = playerField.value;
+   if (isNaN(playerFieldString)) {
+     alert("Please provide Valid Numbers!");
+   } else {
+     const playercost = parseInt(playerFieldString);
      
-          const totalCoastAmount= document.getElementById('final-amount');
-          const totalCoastAmountField=totalCoastAmount.innerText;
-          totalCoastAmount.innerText=totalCoast;
-   
-})
+     const fiveplayerCost =5*playercost;
+ 
+     //    playercost....
+ 
+     const expensesField = document.getElementById("player-expense");
+     const expensesFieldValue = expensesField.innerText;
+     expensesField.innerText = fiveplayerCost;
+   }
+ 
+   // manager cost....
+ 
+   document.getElementById("Total-calc").addEventListener("click", function () {
+      const managerField = document.getElementById("manager");
+      const managerFieldString = managerField.value;
 
-
-
-
-
-
-
-})
+      if(isNaN(managerFieldString)){
+         alert("Please provide Valid Numbers!");
+      }
+     else{
+      const managerCost = parseInt(managerFieldString);
+      managerField.value = managerCost;
+      // console.log(managerCost);
+      //    coach costing
+      const coachField = document.getElementById("coach-cost");
+      const coachFieldString = coachField.value;
+      const coachCoast = parseInt(coachFieldString);
+      coachField.value = coachCoast;
+      const playerExpense = document.getElementById("player-expense");
+      const fiveplayerCost = parseInt(playerExpense.innerText);
+      const totalCoast = fiveplayerCost + managerCost + coachCoast;
+      //    console.log(totalCoast);
+  
+      const totalCoastAmount = document.getElementById("final-amount");
+      const totalCoastAmountField = totalCoastAmount.innerText;
+      totalCoastAmount.innerText = totalCoast;
+     }
+    })
+     
+ });
