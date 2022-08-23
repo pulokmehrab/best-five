e57080925303bf8eb168980343b95f6d2ef0f304
pulokmehrab <khandokarmeherab@gmail.com>
2022-08-23 12:07:41 +0600

    recheck

--- js/utility.js
+++
@@ -1,7 +0,0 @@
-function calculate(){
-    const playerField = document.getElementById('per-player');
-    const playerFieldString =playerField.value;
-    const playercost =parseInt(playerFieldString);
-    playerField.value =playercost;
-
-}
